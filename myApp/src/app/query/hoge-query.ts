0244312aeda623a16371446d353a5fa1e08dac46
ikuosaito1989 <ikuo.saito@classi.jp>
2019-08-30 10:32:17 +0900

    query例を追加

--- myApp/src/app/query/hoge-query.ts
+++ myApp/src/app/query/hoge-query.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HogeStore } from '../store/hoge-store';
 
+// Query is readonly
 @Injectable({
   providedIn: 'root',
 })
 export class HogeQuery extends HogeStore {
-  hoge: string;
+  // You can also use in-class variables
+  // hoge: string;
+
+  get hoge(): string {
+    return `${this.title} ${this.body}`;
+  }
+
+  constructor() {
+    // call parent class
+    super();
+  }
 }
